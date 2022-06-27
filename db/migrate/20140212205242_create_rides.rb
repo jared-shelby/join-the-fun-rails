84473a78cd402fcdefd6cc954c57275f66ef79d9
Jared Shelby <jared.shelby@yale.edu>
2022-06-27 10:02:16 -0400

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration[5.0]
   def change
     create_table :rides do |t|
+      t.references :passenger
+      t.references :taxi
       t.timestamps null: false
     end
   end
